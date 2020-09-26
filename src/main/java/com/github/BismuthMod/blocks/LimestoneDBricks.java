c402261e9093df0c5ceb3715baba99bf9ad9b680
a-j-cabrera <65976395+a-j-cabrera@users.noreply.github.com>
2020-09-25 22:36:45 -0400

    Updated remaining stone tool recipes and added stairs for different stone bricks and stone brick stairs.

---
+++ src/main/java/com/github/BismuthMod/blocks/LimestoneDBricks.java
@@ -0,0 +1,18 @@
+package com.github.BismuthMod.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
+
+public class LimestoneDBricks extends Block {
+
+    public LimestoneDBricks() {
+        super(Properties.create(Material.ROCK)
+                .hardnessAndResistance(1.5f, 6.0f)
+                .sound(SoundType.STONE)
+                .harvestLevel(0)
+                .harvestTool(ToolType.PICKAXE));
+                //.setRequiresTool());
+    }
+}
